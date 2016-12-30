fda33e3c7ede2328d2d43debc2615e6e5d5c3543
Jorge Henrique <jorgehasd@gmail.com>
2016-12-30 20:20:50 -0200

    Revert "Resolvido: Transcribing DNA into RNA"
    This reverts commit f4b4c6c14275d93689fa8c65a90396e2d38a08fb.

--- src/rosalind_clojure/core.clj
+++ src/rosalind_clojure/core.clj
@@ -6,8 +6,6 @@
   [& args]
   (println "Hello, World!"))
 
-;; Função auxiliar
-;; Não é uma resolução
 (defn count-char
   "Recebe um texto e um char,
   retorna a quantidade de vezes que o char se repete no texto."
@@ -20,9 +18,3 @@
   "Problema: Counting DNA Nucleotides."
   [dna]
   (map #(count-char dna %) [\A \C \G \T]))
-
-(defn transcribing-dna-into-rna
-  "Problema: Transcribing DNA into RNA."
-  [dna]
-  (reduce str
-    (map #(if (= % \T) \U %) dna)))
