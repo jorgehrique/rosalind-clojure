f4b4c6c14275d93689fa8c65a90396e2d38a08fb
Jorge Henrique <jorgehasd@gmail.com>
2016-12-30 00:21:27 -0200

    Resolvido: Transcribing DNA into RNA

--- src/rosalind_clojure/core.clj
+++ src/rosalind_clojure/core.clj
@@ -6,6 +6,8 @@
   [& args]
   (println "Hello, World!"))
 
+;; Função auxiliar
+;; Não é uma resolução
 (defn count-char
   "Recebe um texto e um char,
   retorna a quantidade de vezes que o char se repete no texto."
@@ -18,3 +20,9 @@
   "Problema: Counting DNA Nucleotides."
   [dna]
   (map #(count-char dna %) [\A \C \G \T]))
+
+(defn transcribing-dna-into-rna
+  "Problema: Transcribing DNA into RNA."
+  [dna]
+  (reduce str
+    (map #(if (= % \T) \U %) dna)))
