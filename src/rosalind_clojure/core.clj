d11ab86fde00835246f5449c6c2069dd452d68a0
Jorge Henrique <jorgehasd@gmail.com>
2016-12-30 00:06:23 -0200

    Resolvido: Counting DNA Nucleotides

--- src/rosalind_clojure/core.clj
+++ src/rosalind_clojure/core.clj
@@ -5,3 +5,16 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn count-char
+  "Recebe um texto e um char,
+  retorna a quantidade de vezes que o char se repete no texto."
+  [text letter]
+  (count
+    (filter
+      #(= % letter) text)))
+
+(defn counting-dna-nucleotides
+  "Problema: Counting DNA Nucleotides."
+  [dna]
+  (map #(count-char dna %) [\A \C \G \T]))
